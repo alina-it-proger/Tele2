6bf5ce7d45fb1116f3f91b83508480b2033a2d93
alina-it-proger <alinatolkacheva5237@gmail.com>
2024-06-07 01:21:53 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cityButton = document.getElementById('cityName');
-    const modal = document.querySelector('dialog.modal');
-    const confirmButton = document.getElementById('confirmButton');
-    const changeCityButton = document.getElementById('changeCityButton');
+document.addEventListener('DOMContentLoaded', () => {
+    const cityNameButton = document.getElementById('cityName');
+    const cityDialog = document.getElementById('cityDialog');
+    const nameCityParagraph = document.getElementById('nameCity'); 
+    const initialButtons = document.getElementById('initialButtons');
+    const cityListTemplate = document.getElementById('cityListTemplate').content;
 
-    cityButton.addEventListener('click', () => {
-        modal.showModal();
+    cityNameButton.addEventListener('click', () => {
+        initialButtons.style.display = 'block';
+        nameCityParagraph.textContent = 'Ваш город ' + cityNameButton.textContent + '?';
+        cityDialog.showModal();
     });
 
-    confirmButton.addEventListener('click', () => {
-        modal.close();
+    window.confirmCity = function() {
+        cityDialog.close();
+    };
+
+    window.showCityList = function() {
+        initialButtons.style.display = 'none'; 
+        nameCityParagraph.style.display = 'none';
+
+        const clone = document.importNode(cityListTemplate, true);
+        cityDialog.appendChild(clone);
+        cityDialog.querySelector('#citySelection').style.display = 'block';
+    };
+
+    cityDialog.addEventListener('click', event => {
+        if (event.target.classList.contains('city1')) {
+            cityNameButton.innerText = event.target.innerText;
+            closeDialog(); 
+        }
     });
-
-
+    
+    function closeDialog() {
+        const citySelection = cityDialog.querySelector('#citySelection');
+        if (citySelection) {
+            citySelection.remove();
+        }
+        initialButtons.style.display = 'block'; 
+        nameCityParagraph.textContent = 'Ваш город ' + cityNameButton.textContent + '?';
+        nameCityParagraph.style.display = 'block';
+        cityDialog.close();
+    }
 });
